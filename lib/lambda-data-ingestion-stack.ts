6f8e6bdd336dd408a524b302c0b286a32019b256
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2024-03-13 13:13:47 +0300

    Initial commit

---
+++ lib/lambda-data-ingestion-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class LambdaDataIngestionStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'LambdaDataIngestionQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
